6a1b46452a67d406e8f6b915b129c648a508558d
Nicolas Thal <nico.th4l@gmail.com>
2018-05-02 18:04:39 +0200

    update translation for message

--- adback-solution-to-adblock-lite/public/class-ad-back-public.php
+++ adback-solution-to-adblock-lite/public/class-ad-back-public.php
@@ -82,6 +82,43 @@ class Ad_Back_Lite_Public extends Ad_Back_Lite_Generic
 
         $elements = $this->getMyInfo();
 
+        $title = __('To access the website', 'adback-solution-to-adblock-lite');
+        $deactivate = __('Deactivate your adblock<br/>then reload the page.', 'adback-solution-to-adblock-lite');
+        $click = __('Or click on the banner<br/>nearby', 'adback-solution-to-adblock-lite');
+        $close = __('Close', 'adback-solution-to-adblock-lite');
+        $why_title = __('Why do I see this message ?', 'adback-solution-to-adblock-lite');
+        $why_description = __('Your browser blocks the ads on this website. To discover how to access it, find the help down here.', 'adback-solution-to-adblock-lite');
+        $why_link = __('Access help page', 'adback-solution-to-adblock-lite');
+        $issue_title = __('An issue ?', 'adback-solution-to-adblock-lite');
+        $issue_description = __('If you have clicked on the banner or deactivate your adblock and you still see this message, report it to us below.', 'adback-solution-to-adblock-lite');
+        $issue_link = __('Report a bug', 'adback-solution-to-adblock-lite');
+        $issue_test = __('Verification in progress', 'adback-solution-to-adblock-lite');
+        $issue_thank_you = __('Thank you for your help', 'adback-solution-to-adblock-lite');
+        $who_title = __('Who are we ?', 'adback-solution-to-adblock-lite');
+        $who_description = __('Thanks to AdBack, you can monetize your adblocked audience by displaying users friendly banners.', 'adback-solution-to-adblock-lite');
+        $who_link = __('Discover AdBack', 'adback-solution-to-adblock-lite');
+
+        echo <<<JS
+        <script type='text/javascript'>
+            window.adback_wording = {};
+            window.adback_wording['title'] = "{$title}";
+            window.adback_wording['deactivate'] = "{$deactivate}";
+            window.adback_wording['click'] = "{$click}";
+            window.adback_wording['close'] = "{$close}";
+            window.adback_wording['why_title'] = "{$why_title}";
+            window.adback_wording['why_description'] = "{$why_description}";
+            window.adback_wording['why_link'] = "{$why_link}";
+            window.adback_wording['issue_title'] = "{$issue_title}";
+            window.adback_wording['issue_description'] = "{$issue_description}";
+            window.adback_wording['issue_link'] = "{$issue_link}";
+            window.adback_wording['issue_test'] = "{$issue_test}";
+            window.adback_wording['issue_thank_you'] = "{$issue_thank_you}";
+            window.adback_wording['who_title'] = "{$who_title}";
+            window.adback_wording['who_description'] = "{$who_description}";
+            window.adback_wording['who_link'] = "{$who_link}";
+        </script>
+JS;
+
         if (is_array($elements)) {
             foreach ($elements as $type => $element) {
                 if ('product' != $type) {
